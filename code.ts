d192996511edf5a57ec79cc5b8e08609121bc5cf
Steven M. Caruso <51963719+smcaruso@users.noreply.github.com>
2025-09-30 22:14:00 -0400

    UI is working

--- code.ts
+++ code.ts
@@ -1,4 +1,5 @@
 let iframeSize: { height: number, width: number } = { height: 75, width: 960 }
+let frameNode: FrameNode | ComponentNode | InstanceNode | null = null
 
 figma.showUI(__html__, { width: iframeSize.width, height: iframeSize.height })
 // figma.showUI(__html__, { width: 960, height: 560 })
@@ -6,11 +7,11 @@ figma.showUI(__html__, { width: iframeSize.width, height: iframeSize.height })
 figma.on('selectionchange', () => {
 
   const selection = figma.currentPage.selection
-  if (selection.length === 1 && (selection[0].type === 'FRAME' || selection[0].type === 'COMPONENT')) {
-    const frame = selection[0]
-    figma.ui.postMessage({ selection: true, type: frame.type, name: frame.name })
+  if (selection.length === 1 && (selection[0].type === 'FRAME' || selection[0].type === 'COMPONENT' || selection[0].type === 'INSTANCE')) {
+    frameNode = selection[0]
+    figma.ui.postMessage({ msgType: "selection", type: frameNode.type, name: frameNode.name })
   }
-  else {figma.ui.postMessage({ selection: false }) }
+  else {figma.ui.postMessage({ msgType: "clear" }) }
 })
 
 figma.ui.onmessage = msg => {
@@ -21,7 +22,33 @@ figma.ui.onmessage = msg => {
       figma.ui.resize(iframeSize.width, iframeSize.height)
       if (iframeSize.height >= 560) {clearInterval(animateResize)}
     }
+
+    generateCodeForFrame(frameNode)
+
   }
+}
+
+async function generateCodeForFrame(node: FrameNode | ComponentNode | InstanceNode | null) {
+  if (!node) return
+  const className = node.name.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9\-]/g, '')
+  const generatedCode: {html: string, css: string} = { html: `<div class="${className}">sample code</div>`, css: '' }
+
+  frameNode?.getCSSAsync().then(css => {
+    generatedCode.css = `.${className} {\n`
+    for (const prop in css) {
+      if (Object.prototype.hasOwnProperty.call(css, prop)) {
+        const value = (css as Record<string, string>)[prop]
+        generatedCode.css += `  ${prop}: ${value};\n`
+      }
+    }
+    generatedCode.css += `}`
+    figma.ui.postMessage({ msgType: "code", code: generatedCode })
+  })
+
+  figma.variables.getLocalVariablesAsync().then(vars => {
+    console.log('Local variables:', vars)
+  })
+  
 }
 
 // figma.closePlugin()
